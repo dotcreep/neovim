b8cddf2b88d12700848f75760ce81170f79e09fc
dotcreep <github.lent047@esurl.my.id>
2024-08-06 03:28:16 +0700

    Add more convenient plugin

--- lua/buggy/settings/plugin.lua
+++ lua/buggy/settings/plugin.lua
@@ -1,4 +1,10 @@
-require('packer').startup(function(use)
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'                -- Plugin Manager
   ------------------------------------------------------------------------
   use 'windwp/nvim-autopairs'                 -- Auto pair symbols
@@ -7,26 +13,36 @@ require('packer').startup(function(use)
   use 'lukas-reineke/indent-blankline.nvim'   -- Indent blankline
   use 'tpope/vim-fugitive'                    -- Git Intergration
   ------------------------------------------------------------------------
+  --- Highlighting Syntax
+  --- need install 'node' and 'build-essential' atau 'gcc' atau 'base-devel'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require("nvim-treesitter.install").update { with_sync = true } end
+  }
+  ------------------------------------------------------------------------
   --- Themes
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use { "ellisonleao/gruvbox.nvim" }
+  use { 'catppuccin/nvim', as = 'catppuccin' }
+  use 'ellisonleao/gruvbox.nvim'
   use 'Mofiqul/vscode.nvim'
   ------------------------------------------------------------------------
   --- Language Support
   use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
   }
   ------------------------------------------------------------------------
   --- Autocompletions
-  use 'hrsh7th/nvim-cmp'                      -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp'                  -- LSP source for nvim-cmp
-  use 'hrsh7th/cmp-buffer'                    -- Buffer completions
-  use 'hrsh7th/cmp-path'                      -- Path completions
-  use 'hrsh7th/cmp-cmdline'                   -- Command line completions
-  use 'L3MON4D3/LuaSnip'                      -- Snippet engine
-  use 'saadparwaiz1/cmp_luasnip'              -- Snippet completions
-  use 'neovim/nvim-lspconfig'                 -- LSP configurations
+  use {'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',                  -- LSP source for nvim-cmp
+      'hrsh7th/cmp-buffer',                    -- Buffer completions
+      'hrsh7th/cmp-path',                      -- Path completions
+      'hrsh7th/cmp-cmdline',                   -- Command line completions
+      'L3MON4D3/LuaSnip',                      -- Snippet engine
+      'saadparwaiz1/cmp_luasnip'               -- Snippet completions
+    }
+  }
   ------------------------------------------------------------------------
   --- Statusline
   use { 'nvim-lualine/lualine.nvim',
@@ -49,5 +65,5 @@ require('packer').startup(function(use)
     },
     config = true
   }
-
+  if packer_bootstrap then require("packer").sync() end
 end)
