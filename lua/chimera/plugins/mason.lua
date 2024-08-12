8bce20388786831cc4ade025939486047e2e4b7c
dotcreep <github.lent047@esurl.my.id>
2024-08-12 09:10:22 +0700

    Fix mason option for x86_64

--- lua/chimera/plugins/mason.lua
+++ lua/chimera/plugins/mason.lua
@@ -29,22 +29,27 @@ return {
 			"ruby_lsp",
 			"vhdl_ls",
 			"lemminx",
-      "rust_analyzer",
+			"rust_analyzer",
 		}
 		local function add(t, package_name, lsp, linter, dap, formatter)
-			if package(package_name) then
-        if not vim.tbl_contains(not_support_arm64, lsp) then
-				  table.insert(t.lsp, lsp)
-        end
-        if not vim.tbl_contains(not_support_arm64, linter) then
-				  table.insert(t.linter, linter)
-        end
-        if not vim.tbl_contains(not_support_arm64, dap) then
-				  table.insert(t.dap, dap)
-        end
-        if not vim.tbl_contains(not_support_arm64, formatter) then
-				  table.insert(t.formatter, formatter)
-        end
+			if package(package_name) and is_arm64() then
+				if not vim.tbl_contains(not_support_arm64, lsp) then
+					table.insert(t.lsp, lsp)
+				end
+				if not vim.tbl_contains(not_support_arm64, linter) then
+					table.insert(t.linter, linter)
+				end
+				if not vim.tbl_contains(not_support_arm64, dap) then
+					table.insert(t.dap, dap)
+				end
+				if not vim.tbl_contains(not_support_arm64, formatter) then
+					table.insert(t.formatter, formatter)
+				end
+			elseif package(package_name) and not is_arm64() then
+				table.insert(t.lsp, lsp)
+				table.insert(t.linter, linter)
+				table.insert(t.dap, dap)
+				table.insert(t.formatter, formatter)
 			end
 		end
 
